16bcf1dde92aa5597eb322d4dff269cfb0c54849
seikode <64799923+seikode@users.noreply.github.com>
2025-04-02 15:52:33 +0900

    🎨 update

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAy28ywGYatOCNLOIRQXXvZ2UIG5DkNaxU",
+  authDomain: "nwitter-reloaded-3c2ef.firebaseapp.com",
+  projectId: "nwitter-reloaded-3c2ef",
+  storageBucket: "nwitter-reloaded-3c2ef.firebasestorage.app",
+  messagingSenderId: "138651447907",
+  appId: "1:138651447907:web:58f02f19c2486600623e56",
+};
+
+const app = initializeApp(firebaseConfig);
